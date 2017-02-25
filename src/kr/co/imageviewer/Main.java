6ade6ebda417bec237de5948fc1b69a699b6e657
nari <nari@192.168.0.7>
2017-02-26 00:06:17 +0900

    zipUtility 추

--- src/kr/co/imageviewer/Main.java
+++ src/kr/co/imageviewer/Main.java
@@ -1,5 +1,17 @@
 package kr.co.imageviewer;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		ArrayList<String> files = new ArrayList<>();
+		files.add("jquery-3.1.1.min.js");
+		files.add("Stuk-jszip-v3.1.3-1-g5250332.zip");
+//		ZipUtility.filesToZip("/Users/nari/Desktop", files);
+//		System.out.println("zip end");
+		
+		ZipUtility.unzip("/Users/nari/Desktop", "images.zip");
+		System.out.println("unzip end");
+	}
 }
